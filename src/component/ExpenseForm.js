46b474d9c54d144916a9e49797379c604b69869c
Happiness Balogun <happinessblgn0@gmail.com>
2020-05-20 01:51:02 +0100

    styled the app

--- src/component/ExpenseForm.js
+++ src/component/ExpenseForm.js
@@ -66,18 +66,20 @@ export default class ExpenseForm extends React.Component {
   render() {
     return (
       <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
+        {this.state.error && <p className="form__error">{this.state.error}</p>}
+        <form className="form" onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
             autoFocus
+			className="text-input"
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
           <input
             type="text"
             placeholder="Amount"
+			className="text-input"
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
@@ -91,10 +93,11 @@ export default class ExpenseForm extends React.Component {
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
+			className="textarea"
             value={this.state.note}
             onChange={this.onNoteChange}
           ></textarea>
-          <button>Add Expense</button>
+          <button className="button">Add Expense</button>
         </form>
       </div>
     );
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
-//import createHistory from "history/createBrowserHistory";
 import { createBrowserHistory } from "history";
 import AddExpensePage from "../component/AddExpense";
 import EditExpensePage from "../component/EditExpense";
 import ExpenseDashboardPage from "../component/ExpenseDashboard";
-import Header from "../component/Header";
 import NotFoundPage from "../component/404";
 import Login from "../component/Login";
 import PrivateRoute from "./PrivateRoute";
@@ -16,7 +14,6 @@ export const history = createBrowserHistory();
 const AppRouter = (props) => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <PublicRoute exact={true} path="/" component={Login} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
